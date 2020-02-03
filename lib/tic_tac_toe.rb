0a1c8044e5716a9cceb2d68f0017437025f125aa
Steven Perfors <stevecanbuild@gmail.com>
2020-02-03 18:20:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,6 +31,11 @@ class TicTacToe
   end
   
   def turn_count
-    
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
+    end
+  end
   
 end
