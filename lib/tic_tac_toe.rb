3d4aa2d933a67265b31e8b4ed182339e3abf2c5a
Steven Perfors <stevecanbuild@gmail.com>
2020-02-03 18:37:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,11 +57,12 @@ class TicTacToe
     end
   end
   
-  def won? 
+  def won?(board) 
     WIN_COMBINATIONS.detect do |win|
     board[win[0]] == board[win[1]] &&
     board[win[1]] == board[win[2]] &&
-    position_taken?(board, combo[0])
+    position_taken?(board, win[0])
+  end
   
   
   
