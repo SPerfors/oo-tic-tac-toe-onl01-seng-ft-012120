4b75898b1c75cfc66f8cd46b989c65512a789898
Steven Perfors <stevecanbuild@gmail.com>
2020-02-03 18:17:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,9 +50,9 @@ class TicTacToe
     end
   end
   
-  def current_player(board)
-    turn_count(board) % 2 == 0 ? "X" : "O"
-  end
+  #def current_player(board)
+    #turn_count(board) % 2 == 0 ? "X" : "O"
+  #end
     
     
     
